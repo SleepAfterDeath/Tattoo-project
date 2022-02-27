b186c5a1bcee7097ac81edde4ce7828303b2fa82
Mateusz <mporzezinski123@gmail.com>
2022-02-27 16:41:46 +0100

    Add new files

---
+++ main.js
@@ -0,0 +1,7 @@
+<a href="http://127.0.0.1:5500/zadanie%20na%20probe/index.html#meta"><img src="img/1.jpg" onmouseover="this.src = 'img/11.jpg'" onmouseout="this.src = 'img/1.jpg'"></img></a>;
+<a href="http://127.0.0.1:5500/zadanie%20na%20probe/index.html#meta"><img src="img/2.jpg" onmouseover="this.src = 'img/22.jpg'" onmouseout="this.src = 'img/2.jpg'"></img></a>;
+<a href="http://127.0.0.1:5500/zadanie%20na%20probe/index.html#meta"><img src="img/3.jpg" onmouseover="this.src = 'img/33.jpg'" onmouseout="this.src = 'img/3.jpg'"></img></a>;
+<a href="http://127.0.0.1:5500/zadanie%20na%20probe/index.html#meta"><img src="img/4.jpg" onmouseover="this.src = 'img/44.jpg'" onmouseout="this.src = 'img/4.jpg'"></img></a>;
+<a href="http://127.0.0.1:5500/zadanie%20na%20probe/index.html#meta"><img src="img/5.jpg" onmouseover="this.src = 'img/55.jpg'" onmouseout="this.src = 'img/5.jpg'"></img></a>;
+<a href="http://127.0.0.1:5500/zadanie%20na%20probe/index.html#meta"><img src="img/projekt11.jpg" onmouseover="this.src = 'img/projekt2.jpg'" onmouseout="this.src = 'img/projekt11.jpg'"></img></a>;
+
